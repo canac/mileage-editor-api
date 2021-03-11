45ac8b1a688cc313fc6ef1b1dcb5744ee3d2d505
Caleb Cox <canac@users.noreply.github.com>
2021-03-11 16:47:59 -0600

    Restrict access to the user's models

--- src/models/FavoritePlace.ts
+++ src/models/FavoritePlace.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 export default mongoose.model('FavoritePlace', new mongoose.Schema({
+  userId: { type: String, required: true },
   name: { type: String, required: true },
   address: { type: String, required: true },
 }));
