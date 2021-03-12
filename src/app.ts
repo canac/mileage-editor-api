bc87e7b04f6354943c90223a94d132b7576569db
Caleb Cox <canac@users.noreply.github.com>
2021-03-11 18:55:42 -0600

    Set up Auth0 authentication

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,40 @@
 import 'dotenv/config';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import express from 'express';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
 import crudSchema from './crudSchema';
 import { connect } from './db';
 
+const app = express();
+app.use(cors({
+  origin: [process.env.FRONTEND_ORIGIN ?? '', 'https://studio.apollographql.com'],
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  methods: 'GET, POST, PUT, DELETE, OPTIONS',
+}));
+app.use(jwt({
+  secret: expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://mileage-editor.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'https://mileage-editor-api.herokuapp.com',
+  issuer: 'https://mileage-editor.us.auth0.com/',
+  algorithms: ['RS256'],
+}));
+
 const server = new ApolloServer({
   schema: crudSchema,
-  cors: {
-    origin: [process.env.FRONTEND_ORIGIN ?? '', 'https://studio.apollographql.com'],
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-  },
 });
-const port = process.env.PORT;
-connect().then(() => server.listen({ port: port ? parseInt(port, 10) : 3001 })).then(({ url }) => {
+server.applyMiddleware({ app, path: '/' });
+
+const { PORT } = process.env;
+const port = PORT ? parseInt(PORT, 10) : 3001;
+connect().then(() => app.listen({ port })).then(() => {
   // eslint-disable-next-line no-console
-  console.log(`Server running at ${url}`);
+  console.log(`Server running on port ${port}`);
 }).catch((err: Error) => {
   // eslint-disable-next-line no-console
   console.error(err);
