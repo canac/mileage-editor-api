2a881b85bfc1a5332f7a1b8a921f8fb0470e2c17
Caleb Cox <canac@users.noreply.github.com>
2021-03-23 16:28:13 -0500

    Put from field before to field

--- src/models/Journey.ts
+++ src/models/Journey.ts
@@ -4,7 +4,7 @@ export default mongoose.model('Journey', new mongoose.Schema({
   userId: { type: String, required: true },
   date: { type: Date, required: true },
   description: { type: String, required: true },
-  to: { type: String, required: true },
   from: { type: String, required: true },
+  to: { type: String, required: true },
   miles: { type: Number, required: true },
 }));
