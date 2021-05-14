dd7838af73d2c14f9e55eb5e54c2b6bb33d45b58
Caleb Cox <canac@users.noreply.github.com>
2021-05-14 15:16:59 -0500

    Format using Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,38 +5,41 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      files: ['*.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['src/**/*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'import/extensions': 'off',
+        'import/order': [
+          'error',
+          {
+            alphabetize: { order: 'asc' },
+          },
+        ],
+        'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      },
+    },
   ],
-  overrides: [{
-    files: ['*.js'],
-    parserOptions: {
-      sourceType: 'script',
-    },
-  }, {
-    files: ['src/**/*.ts'],
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: 'tsconfig.json',
-    },
-    plugins: [
-      '@typescript-eslint',
-    ],
-    rules: {
-      'import/extensions': 'off',
-      'import/order': ['error', {
-        alphabetize: { order: 'asc' },
-      }],
-      'sort-imports': ['error', { ignoreDeclarationSort: true }],
-    },
-  }],
   rules: {
     'max-len': ['error', { code: 120 }],
     strict: ['error', 'global'],
